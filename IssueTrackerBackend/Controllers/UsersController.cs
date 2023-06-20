05ef7fc30b58c154b84c1055d117e06bc4e921ef
Adam Pietrzak <pietrzakpraca@gmail.com>
2023-06-20 13:26:09 +0200

    New: User model and controller

---
+++ IssueTrackerBackend/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using IssueTrackerBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssueTrackerBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(User user)
+        {
+            // Perform validation checks on the user object
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // TODO: Save the user to the database or perform other registration-related operations
+
+            // Return a success response
+            return Ok(new { Message = "Registration successful" });
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(User user)
+        {
+            // TODO: Implement user authentication logic
+            // Compare the provided username/password with the stored values in the database
+            // Generate and return a token upon successful authentication
+
+            // Placeholder response for demonstration purposes
+            return Ok(new { Message = "Login successful" });
+        }
+    }
+}
